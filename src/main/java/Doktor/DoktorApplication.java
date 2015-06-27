f7ad258adbfdd935a91a1de39a85539a099c418a
pmalneedi <vikky49@gmail.com>
2015-06-26 22:28:58 -0700

    Initial commit

---
+++ src/main/java/Doktor/DoktorApplication.java
@@ -0,0 +1,12 @@
+package Doktor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoktorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoktorApplication.class, args);
+    }
+}
